53dc3a038181dcee5288607f4ba9431d3481f2ea
aminefaouzi39~ <aminefaouzi180@gmail.com>
2025-02-24 21:05:19 +0000

    msg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static export
-  basePath: "/my_affiliate_app", // Must match your GitHub repo name
+  basePath: "/my_affiliate_app", // Your repo name
   assetPrefix: "/my_affiliate_app/",
 };
 
