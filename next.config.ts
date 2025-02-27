ef4f3edf336f5dd821784e7cc02b2bbd8e6c842d
aminefaouzi39~ <aminefaouzi180@gmail.com>
2025-02-27 11:28:25 +0000

    msg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  trailingSlash: false, // Optional, but recommended
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
